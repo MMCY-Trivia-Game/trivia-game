e4b5937999aca4eb3bc6dd42026166cac2e3eb90
mikiyasmebrate <mikiyasmebrate@gmail.com>
2024-12-30 15:41:12 +0300

    feature/middlware/errorhanddler created

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const connectDB = require('./config/dbConnection')
+const errorHandler = require('./middleware/errorHandler')
 
 
 connectDB() //connect database
@@ -8,6 +9,7 @@ const app = express();
 
 //middleware
 app.use(express.json()); // req.body
+app.use(errorHandler)
 
 const port = process.env.PORT || 5000;
 const server = app.listen(port, () => {
