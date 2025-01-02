03e303c04338a211ea31eb66ee620d53b09b5c52
Mikiyas Mebrate <mikiyasmebrate@gmail.com>
2025-01-02 13:55:09 +0300

    flow bite initialized

--- client/src/main.js
+++ client/src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import '../node_modules/flowbite-vue/dist/index.css' //flowbite css 
 
 createApp(App).mount('#app')
