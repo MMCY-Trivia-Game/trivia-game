be03261202abf012daad244e75d6ab38064b907f
mikiyasmebrate <mikiyasmebrate@gmail.com>
2025-01-12 22:42:44 +0300

    list card updated

--- client/src/components/admin-component/ui/GameListCard.vue
+++ client/src/components/admin-component/ui/GameListCard.vue
@@ -1,11 +1,11 @@
 <template>
-    <fwb-card class="p-2 hover:bg-slate-200 transform transition-transform duration-300 hover:scale-105" :img-alt="title"
-        :img-src="image" variant="image">
-        <div class="p-2">
+    <div
+        class="flex flex-col p-5 bg-secondary border border-white text-white justify-between h-40 rounded-md transform transition-transform duration-300 hover:scale-105">
+        <div class="">
             <fwb-tooltip>
                 <template #trigger>
                     <button>
-                        <p class="font-normal w-20 text-gray-700 dark:text-gray-400 truncate overflow-hidden">
+                        <p class="flex-grow text-start overflow-hidden text-ellipsis line-clamp-2">
                             {{ title }}
                         </p>
                     </button>
@@ -14,14 +14,29 @@
                     {{ title }}
                 </template>
             </fwb-tooltip>
-            <fwb-badge :type="active ? 'green' : 'default'">{{ active ? 'Active' : 'Not Active' }}</fwb-badge>
         </div>
-    </fwb-card>
+
+
+        <div class="text-gray-400">
+            By {{ creator }}
+        </div>
+        <div>
+            <span v-if="active"
+                class="inline-flex items-center justify-center w-6 h-6 me-2 text-sm font-semibold bg-green-400 rounded-full">
+            </span>
+            <span v-else
+                class="inline-flex items-center justify-center w-6 h-6 me-2 text-sm font-semibold bg-red-400 rounded-full">
+            </span>
+        </div>
+
+    </div>
 </template>
 
+
+
 <script setup>
-import { FwbCard, FwbTooltip, FwbBadge, FwbButton } from 'flowbite-vue'
-const { title, image, active } = defineProps(['title', 'image', 'active'])
+import { FwbTooltip } from 'flowbite-vue'
+const { title, image, active, creator } = defineProps(['title', 'image', 'active', 'creator'])
 
 
 
--- client/src/pages/admin/Games.vue
+++ client/src/pages/admin/Games.vue
@@ -1,5 +1,5 @@
 <template>
-    <h3 class="text-primary  font-bold text-2xl">Users</h3>
+    <h3 class="text-white pb-6 font-bold text-2xl">Games</h3>
     <!--Search game-->
     <section class="w-full md:w-1/3">
         <label for="search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
@@ -7,7 +7,8 @@
             <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
                 <MagnifyingGlassIcon class="text-primary h-4 w-4" />
             </div>
-            <input type="search" id="search"
+
+            <input type="search" v-model="searchData"
                 class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-primary focus:border-primary dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary dark:focus:border-primary"
                 placeholder="Search" required />
             <div class="absolute end-2.5 bottom-2.5">
@@ -27,10 +28,10 @@
     </section>
 
     <!--Game List-->
-    <section class=" mt-5 grid grid-cols-3 gap-4 mb-4 md:grid-cols-4 lg:grid-cols-8">
+    <section class=" mt-5 grid grid-cols-3 gap-4 mb-4 md:grid-cols-4 lg:grid-cols-6 h-full">
         <GameListCard v-if="!gameStore.isLoading && gameStore.games.length > 0" v-for="game in gameStore.games"
-            @click="toggleModal" :title="game.title" :active="game.is_active"
-            :image="gameImages[Math.floor(Math.random() * gameImages.length)]" />
+            :key="game.id" @click="toggleModal" :title="game.title" :active="game.is_active"
+            :creator="game.creator_id.first_name + ' ' + game.creator_id.first_name" />
     </section>
 
     <!-- No game found message -->
@@ -49,7 +50,7 @@
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 
 import GameListCard from '@/components/admin-component/ui/GameListCard.vue';
 import { MagnifyingGlassIcon } from '@heroicons/vue/24/outline';
@@ -73,10 +74,17 @@ import { onMounted } from 'vue';
 const gameStore = useGameStore()
 const isShowModal = ref(false)
 const selectedCategory = ref('All')
+const searchData = ref('')
 
 onMounted(async () => {
     await gameStore.fetchGames()
 })
+
+const filteredGames = computed(() => {
+    return gameStore.games.filter(game =>
+        game.title.toLowerCase().includes(searchData.value.toLowerCase())
+    );
+});
 
 const toggleModal = () => {
     isShowModal.value = !isShowModal.value
@@ -101,6 +109,11 @@ const categories = [
 const handleOnCategorySelected = async (category) => {
     selectedCategory.value = category
     await gameStore.fetchGames(category)
+}
+
+//on search handler
+const onSearch = async () => {
+    await userStore.fetchUsers(1, searchData.value);
 }
 </script>
 
