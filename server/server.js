5feaed2c4e0389c89aee7f1190a867e0afabf824
Kaleab Samuel <kaleabsamuel55@gmail.com>
2025-01-28 08:17:29 +0300

    testing 5

--- server/server.js
+++ server/server.js
@@ -218,21 +218,21 @@ io.on('connection', (socket) => {
   });
 });
 
-// if (process.env.NODE_ENV && process.env.NODE_ENV === 'production') {
-//   const __dirname = path.resolve();
-//   // app.use('/uploads', express.static('/var/data/uploads'));
-//   app.use(express.static(path.join(__dirname, '/client/build')));
+if (process.env.NODE_ENV && process.env.NODE_ENV === 'production') {
+  const __dirname = path.resolve();
+  // app.use('/uploads', express.static('/var/data/uploads'));
+  app.use(express.static(path.join(__dirname, '/client/dist')));
 
-//   app.get('*', (req, res) =>
-//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-//   );
-// } else {
-//   // const __dirname = path.resolve();
-//   // app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
-//   // app.get('/', (req, res) => {
-//   //   res.send('API is running....');
-//   // });
-// }
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'))
+  );
+} else {
+  // const __dirname = path.resolve();
+  // app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+  // app.get('/', (req, res) => {
+  //   res.send('API is running....');
+  // });
+}
 
 const port = process.env.PORT || 5000;
 httpServer.listen(port, () => {
